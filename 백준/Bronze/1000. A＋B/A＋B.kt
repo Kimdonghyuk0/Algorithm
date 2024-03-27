55d19899198163ccd4dfb3571a8c7efa61c11133
Kimdonghyuk0 <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-03-27 18:48:47 +0900

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun sum() {
+    val scanner = Scanner(System.`in`)
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+    
+    println(a+b)
+}
+
+fun main() {
+    sum()
+}
