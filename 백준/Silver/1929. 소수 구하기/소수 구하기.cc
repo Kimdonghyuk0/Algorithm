b5c25f342fbc0fe7a85a41de6f4597312e10a519
Kimdonghyuk0 <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-05-06 20:18:56 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2884 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,29 +1,52 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int arr[1000001];
-int main(void)
-{
-	int n, m;
-	cin >> n >> m;
-	for (int i = 2; i <= m; i++)
-		arr[i] = i;
+bool checkPrimeNumber(int num) {
+    if (num == 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
 
-	for (int i = 2; i <= sqrt(m); i++)		
-	{
-		if (arr[i] == 0)		
-			continue;
-		for (int j = i * i; j <= m; j += i)		
-			arr[j] = 0;
-	}
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) return false;
+    }
 
-	for (int i = n; i <= m; i++)
-	{
-		if (arr[i] == 0)
-			continue;
-		else
-			cout << arr[i] << '\n';
-	}
-	return 0;
+    return true;
+}
+
+int main() {
+    int M, N;
+    cin >> M >> N;
+
+    int size = N - M + 1;
+    bool* prime = new bool[size];
+    for (int i = 0; i < size; ++i) {
+        prime[i] = true;
+    }
+
+    if (M == 1) {
+        prime[0] = false;
+    }
+
+  
+    int limit = static_cast<int>(sqrt(N)) + 1;
+    for (int i = 2; i < limit; ++i) {
+        if (checkPrimeNumber(i)) {
+            int start = max(i * i, (M + i - 1) / i * i);
+            for (int j = start; j <= N; j += i) {
+                prime[j - M] = false;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (prime[i]) {
+            cout << M + i << "\n";
+        }
+    }
+
+    delete[] prime;
+
+    return 0;
 }
