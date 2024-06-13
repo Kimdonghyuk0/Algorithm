306ba71b862e96545f9f29a7abe29195b7e5c3bc
DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-06-13 19:03:03 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int A,B,V;
+
+	
+	cin>> A>>B>>V ;
+	
+	if((V-B)%(A-B)==0){
+		cout<<(V-B)/(A-B);
+		return 0;
+	}
+	else {
+			cout<<(V-B)/(A-B)+1;
+	}
+	
+ 
+ 
+}
