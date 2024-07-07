694264ac7b59b21f965ee2b810584b86183fbcba
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-07-07 22:44:45 +0900

    [Silver IV] Title: solved.ac, Time: 76 ms, Memory: 3240 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<cmath>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main() {
+	double n;
+	int e; //제거해야할 사람수 위,아래 각각
+	cin >> n;
+	deque<int> dq;
+	if (n == 0) {
+		cout << "0";
+		return 0;
+	}
+
+	e = round(n * 0.15);
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		dq.push_back(num);
+	}
+	sort(dq.begin(), dq.end());
+	for (int  i = 0; i < e; i++)
+	{
+		dq.pop_back();
+		dq.pop_front();
+	}
+	double sum = 0.0;
+	for (int i = 0; i < dq.size(); i++)
+	{
+		sum += dq[i];
+	}
+	int result = round(sum / dq.size());
+	cout << result;
+
+
+
+	return 0;
+}
