55af3c78f05c877b9f16ae1f2cf4eccd2145be3b
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-08-30 15:52:04 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -7,9 +7,9 @@ int main() {
 	cin >> s;
 	int zero= 0;
 	int one = 0;
-	s += '9';
 
-	for (int i = 0; i < s.length()-1; i++)
+
+	for (int i = 0; i < s.length(); i++)
 	{
 		if (s[i] != s[i + 1]) {
 			if (s[i] == '0')zero++;
