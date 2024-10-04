93ddbc6dcef2a9e19c607a7a3980fd0d3fd25273
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-10-04 12:13:58 +0900

    [Silver IV] Title: 크림 파스타, Time: 1508 ms, Memory: 81592 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.java
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.java
@@ -10,15 +10,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
-        int arr[] = new int[size];
         String input[] = br.readLine().split(" ");
         int min = Integer.MAX_VALUE;
         int max;
         int ans = 0;
         for (int i = 0; i < size; i++) {
-            arr[i] = Integer.parseInt(input[i]);
-            max = arr[i];
-            if (arr[i] < min) min = arr[i];
+           
+            max = Integer.parseInt(input[i]);
+            if (Integer.parseInt(input[i]) < min) min = Integer.parseInt(input[i]);;
             if (ans < max - min) ans = max - min;
             System.out.print(ans + " ");
         }
