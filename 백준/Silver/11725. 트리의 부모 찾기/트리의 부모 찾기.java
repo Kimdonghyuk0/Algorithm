6bcf8ef5377af44c71e4bb81ae2ab4813df1d37b
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2024-11-22 00:14:24 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1576 ms, Memory: 86748 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -8,26 +8,14 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-    private static Queue<Integer> q = new LinkedList<>();
-    private static ArrayList<Integer>[] graph;
-    private static int parent[];
-    private static boolean visited[];
 
-    private static void bfs(int start) {
-        q.add(start);
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-            visited[temp] = true;
-            for (int nextV : graph[temp]) {
-                if (!visited[nextV]) {
-                    q.add(nextV);
-                    parent[nextV] = temp;
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
+
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer>[] graph;
+        int parent[];
+        boolean visited[];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         graph = new ArrayList[n + 1];
@@ -43,10 +31,23 @@ public class Main {
             graph[n1].add(n2);
             graph[n2].add(n1);
         }
-        bfs(1);
+        q.add(1);
+        visited[1] = true;
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            for (int nextV : graph[temp]) {
+                if (!visited[nextV]) {
+                    q.add(nextV);
+                    visited[nextV] = true;
+                    parent[nextV] = temp;
+                }
+            }
+        }
         for (int i = 2; i < n + 1; i++) {
             System.out.println(parent[i]);
         }
     }
+
+
 }
 
