99fe10f8d9cc828b83a022793649af38dfed8870
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2025-02-07 14:41:52 +0900

    [Silver III] Title: N과 M (7), Time: 560 ms, Memory: 220680 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Solution {
+    private int n, m;
+    private int[] numbers;
+    private int[] ans;
+    StringBuilder sb = new StringBuilder();
+
+    private void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int val : ans) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            ans[depth] = numbers[i];
+            dfs(i + 1, depth + 1);
+        }
+    }
+
+    public String solve(int n, int m, int[] numbers) {
+        Arrays.sort(numbers);
+        ans = new int[m];
+        this.numbers = numbers;
+        this.n = n;
+        this.m = m;
+        dfs(0, 0);
+
+        return sb.toString();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N, M;
+        int[] numbers;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            numbers[i] = n;
+        }
+        Solution solution = new Solution();
+        System.out.println(solution.solve(N, M, numbers));
+
+    }
+}
