0f2169ffed7d62fc25858eb70f9fe7309f346edd
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2025-02-10 21:06:46 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 104 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.util.Collections.reverseOrder;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        Queue<Character> q = new LinkedList<>();
+        for (int i = 0; i < N.length(); i++) {
+            q.add(N.charAt(i));
+        }
+        int ans = 0;
+        while (!q.isEmpty()) {
+            ans++;
+            String number = Integer.toString(ans);
+            for (int i = 0; i < number.length(); i++) {
+                char n = number.charAt(i);
+                if (q.isEmpty()) break;
+                if (q.peek() == n) {
+                    q.poll();
+                }
+
+            }
+        }
+        System.out.print(ans);
+        br.close();
+
+    }
+}
