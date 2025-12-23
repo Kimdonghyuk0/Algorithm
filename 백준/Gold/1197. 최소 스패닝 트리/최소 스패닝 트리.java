744d7b4b739c04d50c6cf299b6c72f89c39990a6
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2025-12-23 17:38:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 556 ms, Memory: 51072 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int total = 0;
+    static int[] parent;
+
+    static class Node implements Comparable<Node>{
+        int to;
+        int from;
+        int cost;
+
+        public Node(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static boolean union(int x, int y){
+        int xp = find(x);
+        int yp = find(y);
+        if(xp==yp)return false;
+        if(xp<=yp)parent[yp] = xp;
+        else parent[xp] = yp;
+        return true;
+    }
+    static int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Node> list = new ArrayList<>();
+        int N,M;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 부모 배열 초기화
+        parent = new int[N+1];
+        for(int i = 0; i<N+1; i++){
+            parent[i] = i;
+        }
+        // 양방향임
+        for(int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list.add(new Node(from, to, cost));
+        }
+        Collections.sort(list); //cost기준으로 오름차순 정렬
+        int sum = 0;
+        int cnt = 0;
+        for(Node node:list){
+            // 만약 사이클이 없다면 추가
+            if(union(node.from,node.to)){
+                sum+=node.cost;
+                cnt++;
+            }
+            if(cnt==N-1)break;
+        }
+        System.out.println(sum);
+    }
+}
