dd11b0d0451ce67040144bfba06e70e6a06b1233
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-01-22 09:33:35 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 660 ms, Memory: 241860 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,18 +1,20 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0; i<N; i++){
-            String[] input  = br.readLine().split(" ");
-            sb.append(Integer.parseInt(input[0])+Integer.parseInt(input[1])).append("\n");
-        }
-        System.out.println(sb);
-        br.close();
-    }
 
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append('\n');
+        }
+
+        System.out.print(sb);
+    }
 }
