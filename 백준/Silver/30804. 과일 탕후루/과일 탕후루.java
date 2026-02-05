e65a4dd5d1eca092f02386ad3a9d4c3ef81270eb
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-02-05 11:24:38 +0900

    [Silver II] Title: 과일 탕후루, Time: 212 ms, Memory: 27372 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,43 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int arr[] = new int[N];
-        int freq[] = new int[10];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int MaxCount = 0;
-        int count = 0;
-        int left = 0;
-        int right = 0;
-        int v = 0; //종류
-        while (right < N) {
-            int vc = 0;
-            if (v <= 2) freq[arr[right]]++;
-            for (int i = 0; i < freq.length; i++) {
-                if (freq[i] != 0) vc++;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] tangs = new int[N];
+            int[] freq = new int[10];
+            for(int i = 0; i<N; i++){
+                tangs[i] = Integer.parseInt(st.nextToken());
             }
-            v = vc;
-            if (v <= 2) {
-                count++;
-                right++;
-            } else {
-                if (MaxCount < count) MaxCount = count;
-                freq[arr[left]]--;
-                left++;
-                count = right - left;
+            int right = 0;
+            int left = 0;
+            int max = 0;
+            int v = 0;
+            while(right<N){
+                int vc = 0;
+                if(v<=2)freq[tangs[right]]++;
+
+                for(int i = 1; i<=9; i++){
+                    if(freq[i]>0)vc++;
+                }
+                v = vc;
+                if(vc<=2){
+                    if(max<right-left+1)max = right-left+1;
+
+                    right++;
+                }
+                else {
+                    freq[tangs[left]]--;
+                    left++;
+                }
             }
+            System.out.println(max);
+
+
         }
-        if (MaxCount < count) MaxCount = count;
-        System.out.println(MaxCount);
-    }
+
 }
