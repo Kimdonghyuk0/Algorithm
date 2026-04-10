7e0d303142cb34974227c05f23c95fcf3472198c
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-04-10 23:50:02 +0900

    [Gold IV] Title: 알고스팟, Time: 76 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,64 +1,74 @@
 import java.io.*;
 import java.util.*;
 
+class Node {
+    int y, x;
 
-class Node{
-    int x,y,cost;
-    public Node(int y, int x, int cost){
-        this.x = x;
+    public Node(int y, int x) {
         this.y = y;
-        this.cost = cost;
+        this.x = x;
     }
 }
+
 public class Main {
-    static int n,m;
+    static int n, m;
     static int[][] map;
     static int[][] dist;
-    static int[] dy = {1,-1,0,0};
-    static int[] dx = {0,0,1,-1};
-    public static void main(String[] args) throws Exception {
+    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 0, 1, -1};
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        map = new int[n+1][m+1];
-        dist = new int[n+1][m+1];
+
+        map = new int[n + 1][m + 1];
+        dist = new int[n + 1][m + 1];
+
         for (int i = 1; i <= n; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-        //1,1->n,m 가는데 최소비용
-        for(int i = 1; i<=n; i++){
-            String[] input = br.readLine().split("");
-            for(int j = 1; j<=m; j++){
-                map[i][j] = Integer.parseInt(input[j-1]);
+
+        for (int i = 1; i <= n; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = line.charAt(j - 1) - '0';
             }
         }
-        dijkstra();
-        System.out.println(dist[n][m]);
 
+        bfs01();
+
+        System.out.println(dist[n][m]);
     }
 
-    private static void dijkstra() {
-
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->{
-            return a.cost-b.cost;
-        });
+    static void bfs01() {
+        Deque<Node> deque = new ArrayDeque<>();
         dist[1][1] = 0;
-        pq.add(new Node(1,1,0));
-        while(!pq.isEmpty()){
-            Node now = pq.poll();
-            if(now.cost>dist[now.y][now.x])continue;
-            for(int i = 0; i<4; i++){
-                if(now.y+dy[i]<1||now.y+dy[i]>n||now.x+dx[i]<1||now.x+dx[i]>m)continue;
-                int nextW = map[now.y+dy[i]][now.x+dx[i]];
-                if((nextW+now.cost)<dist[now.y+dy[i]][now.x+dx[i]]){
-                    dist[now.y+dy[i]][now.x+dx[i]] = nextW+now.cost;
-                    pq.add(new Node(now.y+dy[i],now.x+dx[i],nextW+now.cost));
+        deque.addFirst(new Node(1, 1));
+
+        while (!deque.isEmpty()) {
+            Node now = deque.pollFirst();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+
+                if (ny < 1 || ny > n || nx < 1 || nx > m) continue;
+
+                int cost = map[ny][nx];
+
+                if (dist[now.y][now.x] + cost < dist[ny][nx]) {
+                    dist[ny][nx] = dist[now.y][now.x] + cost;
+
+                    if (cost == 0) {
+                        deque.addFirst(new Node(ny, nx));
+                    } else {
+                        deque.addLast(new Node(ny, nx));
+                    }
                 }
             }
         }
-
-
     }
 }
