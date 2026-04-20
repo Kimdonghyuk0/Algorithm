5c55a11233b5e16b31e8f9a7540b88da9897959c
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-04-21 04:09:38 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+
+        long answer = Math.min(M, K) + Math.min(N - M, N - K);
+
+        System.out.println(answer);
+    }
+}
