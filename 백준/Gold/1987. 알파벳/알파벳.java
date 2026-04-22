a363c7391a9114f0e06adb895063e84787a0b9af
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-04-22 17:05:06 +0900

    [Gold IV] Title: 알파벳, Time: 2152 ms, Memory: 296640 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,47 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-// 세트를 사용한 DFS로 풀면 편하겠다.
-
 public class Main {
-    static int R,C;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static String[][] board;
-    static boolean[][] visited;
+    static int R, C;
+    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 0, 1, -1};
+    static char[][] board;
+    static Set<Character> set = new HashSet<>();
     static int ans = 1;
 
-    static Set<String> set = new HashSet<>();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        board = new String[R][C];
-        visited = new boolean[R][C];
-        for(int i = 0; i<R; i++){
-            String[] input = br.readLine().split("");
-            board[i] = input;
+        board = new char[R][];
+        for (int i = 0; i < R; i++) {
+            board[i] = br.readLine().toCharArray();
         }
-        dfs(0,0,1);
+        
+        set.add(board[0][0]);  // 시작 셀 알파벳 선추가
+        dfs(0, 0, 1);
         System.out.println(ans);
     }
-    static void dfs(int y, int x,int count){
-        int before = set.size();
-        set.add(board[y][x]);
-        int after = set.size();
-        if(before==after)return;
-        if(count>ans)ans = count;
-        for(int i = 0; i<4; i++){
-            int nextY = y+dy[i];
-            int nextX = x+dx[i];
-            if(nextX<0||nextY<0||nextY>=R||nextX>=C||visited[nextY][nextX])continue;
-            visited[nextY][nextX] = true;
-            dfs(nextY,nextX,count+1);
-            visited[nextY][nextX] = false;
+
+    static void dfs(int y, int x, int count) {
+        ans = Math.max(ans, count);
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+            if (set.contains(board[ny][nx])) continue;  // 이미 지난 알파벳
+            
+            set.add(board[ny][nx]);
+            dfs(ny, nx, count + 1);
+            set.remove(board[ny][nx]);
         }
-        set.remove(board[y][x]);
-
     }
-
 }
