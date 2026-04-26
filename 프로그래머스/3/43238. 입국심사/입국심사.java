85b4e7ee717f237fede6d556f900685cf3503777
_DONGHYUK <127577430+Kimdonghyuk0@users.noreply.github.com>
2026-04-26 14:03:36 +0900

    [level 3] Title: 입국심사, Time: 52.76 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long max = 0;
+        for(int nn:times)if(max<nn)max = nn;
+        long left = 0;
+        long right = max*(long)n;
+        long answer = 0;
+        while(left<=right){
+            long mid = (left+right)/2;
+            long timeSum = 0;
+            for(int time:times){
+                timeSum+=mid/time;
+            }
+            if(timeSum>=n){
+                answer = mid;
+                right = mid-1;
+            }
+            else left = mid+1;
+        }
+        return answer;
+    }
+}
+
+// 끝날때마다 바로 들어갈 지, 기다렸다가 다른 사람한테 받을지
+// 다음 사람 예상 끝나는 시간을 배열에 추가한다. 그리고 그게 가장 작은 곳부터 넣는다.
+// 우선순위 큐를 쓰자
